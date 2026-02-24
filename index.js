76536ce36b06fe752dc15d4ff1e735fb8c40b36a
heit00 <heitorutyama@gmail.com>
2026-02-23 23:57:22 -0300

    refactor/organized files

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
-const {CaseClause:cc, Query:qb, CaseClause, Column} = require('./lib/index');
-module.exports = qb;
-
-const y = qb.case();
-y.when('x','=','y', 2).when('u','z',4)
-const x = new qb().select('2').whereIn('2', ['1','2']).orWhereIn('k', ['1' ,'2' , qb.column('a.b')]).where(2,3).join('z','k', '=', y).from('k');
-
-console.log(x.toInstruction());
-=======
-const qb = require('./lib/index');
-module.exports = qb
->>>>>>> c299921584efb45f8278d0dcdcd939e7d97e7123
-
-/*
- OUTPUT: 
- {
-  template: 'DELETE FROM "a" WHERE ("x" = "z" AND ("x" = $1 OR "x" = "r"))',
-  values: [ 2 ]
-}
-*/
-
+const Query = require('./lib/index');
+const qb = Query;
+module.exports = { qb };
 
 
